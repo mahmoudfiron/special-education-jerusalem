1d9603881f91e027c6f7500b3eef2c4f80aba598
mahmoudfiron <mahmoudfiron13@gmail.com>
2024-06-11 17:46:51 +0300

    i dont know wtf is this

---
+++ special-education-jerusalem/special-education-jerusalem/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Yoo0oo98sodooخ</h1>
+    </div>
+  );
+}
+
+export default App;
--- special-education-jerusalem/src/App.js
+++ special-education-jerusalem/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className='App'>
    
-    <h1>Yosdsdjjjdff</h1>
+    <h1>Yosdsdjjjyoodff</h1>
 
     </div>
   );
